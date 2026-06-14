bcbddf3fe36f3bdcb0742a684802869bdf2a4532
utsav-kumar-droid <uku6396@gmail.com>
2026-06-14 08:56:33 +0530

    Connected frontend to Render backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,10 +5,14 @@ const Contact = require("./models/Contact");
 require("dotenv").config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "https://portfolio-gilt-delta-96.vercel.app" // 👈 Replace with your actual live Vercel URL
+}));
 app.use(express.json());
+
 
 
 // MongoDB connection
@@ -46,6 +50,6 @@ app.post("/contact", async (req, res) => {
 });
 
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
